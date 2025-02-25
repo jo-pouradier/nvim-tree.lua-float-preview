d8dd87f8f7b397d79a026177da96d68398d1187a
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2025-02-25 23:03:26 +0100

    fix: create user command via auto command

--- lua/float-preview.lua
+++ lua/float-preview.lua
@@ -328,10 +328,15 @@ function FloatPreview:attach(bufnr)
     self:preview_under_cursor()
   end
 
-  vim.api.nvim_create_user_command("ToggleFilePreview", toggle_preview, { buffer = bufnr })
-
   local au = {}
-
+  table.insert(
+    au,
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        vim.api.nvim_create_user_command("TogglePreviewFile", toggle_preview, { buffer = bufnr })
+      end,
+    })
+  )
   table.insert(
     au,
     vim.api.nvim_create_autocmd({ "CursorHold" }, {
